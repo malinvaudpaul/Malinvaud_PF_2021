032b38d7ed70958a5eab096ce1bbddeca54f9549
malinvaudpaul <paulmalinvaud@protonmail.com>
2021-10-25 16:56:56 +0200

    Revue de App

--- TD2/app/src/main/java/td2/exercice2/App.java
+++ TD2/app/src/main/java/td2/exercice2/App.java
@@ -3,23 +3,25 @@ package td2.exercice2;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
-public class App {
+public class App<T> {
+    Predicate<Paire<Integer, Double>> tropPetite = p -> p.fst < 100;
+    Predicate<Paire<Integer, Double>> tropGrande = p -> p.fst > 200;
+    Predicate<Paire<Integer, Double>> tailleIncorrecte = p -> tropPetite.test(p) || tropGrande.test(p);
+    Predicate<Paire<Integer, Double>> tailleCorrecte = Predicate.not(tailleIncorrecte);
+    Predicate<Paire<Integer, Double>> tropLourd = p -> p.snd > 150.0;
+    Predicate<Paire<Integer, Double>> poidsCorrect = Predicate.not(tropLourd);
+    Predicate<Paire<Integer, Double>> accesAutorise = p -> tailleCorrecte.test(p) && poidsCorrect.test(p);
+
     public static void main(String[] args) {
-        Predicate<Paire<Integer, Double>> tropPetite = p -> p.fst < 100;
-        Predicate<Paire<Integer, Double>> tropGrande = p -> p.fst > 200;
-        Predicate<Paire<Integer, Double>> tailleIncorrecte = p -> tropPetite.test(p) || tropGrande.test(p);
-        Predicate<Paire<Integer, Double>> tailleCorrecte = Predicate.not(tailleIncorrecte);
-        Predicate<Paire<Integer, Double>> tropLourd = p -> p.snd > 150.0;
-        Predicate<Paire<Integer, Double>> poidsCorrect = Predicate.not(tropLourd);
-        Predicate<Paire<Integer, Double>> accesAutorise = p -> tailleCorrecte.test(p) && poidsCorrect.test(p);
+        App app = new App<>();
 
         Paire bobYes = new Paire(150, 100.0); // taille: 150, poids: 100.0 -> ok
         Paire bobNo = new Paire(250, 100.0); // taille: 250, poids: 100.0 -> trop Grand
         Paire bobFat = new Paire(150, 200.0); // taille: 150, poids: 200.0 -> trop Lourd
 
-        System.out.println(accesAutorise.test(bobYes));
-        System.out.println(accesAutorise.test(bobNo));
-        System.out.println(accesAutorise.test(bobFat));
+        System.out.println(app.accesAutorise.test(bobYes));
+        System.out.println(app.accesAutorise.test(bobNo));
+        System.out.println(app.accesAutorise.test(bobFat));
 
     }
 }
---
+++ TD2/app/src/main/java/td2/universite/App.java
@@ -0,0 +1,27 @@
+package td2.universite;
+
+import java.util.Map;
+import java.util.Set;
+
+public class App {
+    public static void main(String[] args) {
+        Matiere m1 = new Matiere("MAT1");
+        Matiere m2 = new Matiere("MAT2");
+        UE ue1 = new UE("UE1", Map.of(m1, 2, m2, 2));
+        Matiere m3 = new Matiere("MAT3");
+        UE ue2 = new UE("UE2", Map.of(m3, 1));
+        Annee a1 = new Annee(Set.of(ue1, ue2));
+        Etudiant e1 = new Etudiant("39001", "Alice", "Merveille", a1);
+        e1.noter(m1, 12.0);
+        e1.noter(m2, 14.0);
+        e1.noter(m3, 10.0);
+        System.out.println(e1);
+        Etudiant e2 = new Etudiant("39002", "Bob", "Eponge", a1);
+        e2.noter(m1, 14.0);
+        e2.noter(m3, 14.0);
+        Etudiant e3 = new Etudiant("39003", "Charles", "Chaplin", a1);
+        e3.noter(m1, 18.0);
+        e3.noter(m2, 5.0);
+        e3.noter(m3, 14.0);
+    }
+}
