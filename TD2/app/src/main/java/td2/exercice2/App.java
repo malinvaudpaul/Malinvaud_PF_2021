649431470380367f442c9fa14c1c88da49c47781
malinvaudpaul <paulmalinvaud@protonmail.com>
2021-10-25 16:52:47 +0200

    Passage à l'exercice 3

--- TD2/app/src/main/java/td2/exercice2/App.java
+++ TD2/app/src/main/java/td2/exercice2/App.java
@@ -1,5 +1,6 @@
 package td2.exercice2;
 
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class App {
@@ -10,14 +11,15 @@ public class App {
         Predicate<Paire<Integer, Double>> tailleCorrecte = Predicate.not(tailleIncorrecte);
         Predicate<Paire<Integer, Double>> tropLourd = p -> p.snd > 150.0;
         Predicate<Paire<Integer, Double>> poidsCorrect = Predicate.not(tropLourd);
-        Predicate<Paire<Integer, Double>> accèsAutorisé = p -> tailleCorrecte.test(p) && poidsCorrect.test(p);
+        Predicate<Paire<Integer, Double>> accesAutorise = p -> tailleCorrecte.test(p) && poidsCorrect.test(p);
 
         Paire bobYes = new Paire(150, 100.0); // taille: 150, poids: 100.0 -> ok
         Paire bobNo = new Paire(250, 100.0); // taille: 250, poids: 100.0 -> trop Grand
         Paire bobFat = new Paire(150, 200.0); // taille: 150, poids: 200.0 -> trop Lourd
 
-        System.out.println(accèsAutorisé.test(bobYes));
-        System.out.println(accèsAutorisé.test(bobNo));
-        System.out.println(accèsAutorisé.test(bobFat));
+        System.out.println(accesAutorise.test(bobYes));
+        System.out.println(accesAutorise.test(bobNo));
+        System.out.println(accesAutorise.test(bobFat));
+
     }
 }
